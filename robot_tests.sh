bfdfc77290e628e38a2db9a28f69004ae02ee96b
“DaryaShalukho” <shalukhofamily1@gmail.com>
2024-06-09 20:19:29 +0200

    pabot improvement

--- robot_tests.sh
+++ robot_tests.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Создаем директории для результатов
-mkdir -p 03_results/chrome
-mkdir -p 03_results/firefox
+mkdir -p results/chrome
+mkdir -p results/firefox
 
-# Запускаем тесты для Chrome
-pabot --processes 1 -v BROWSER:chrome -V chrome_args.txt --outputdir 03_results/chrome 02_tests
-
-# Запускаем тесты для Firefox
-pabot --processes 1 -v BROWSER:firefox -V firefox_args.txt --outputdir 03_results/firefox 02_tests
+pabot --processes 1 -v BROWSER:chrome -V chrome_args.txt --outputdir results/chrome tests
+pabot --processes 1 -v BROWSER:firefox -V firefox_args.txt --outputdir results/firefox tests
 
 
